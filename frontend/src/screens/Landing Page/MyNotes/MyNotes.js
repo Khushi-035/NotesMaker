bf43804f391028d97bf1e2121770c9a5527f3442
Khushi Sachan <sachan.khushi02@gmail.com>
2023-05-26 18:57:14 +0530

    Data Added

--- frontend/src/screens/Landing Page/MyNotes/MyNotes.js
+++ frontend/src/screens/Landing Page/MyNotes/MyNotes.js
@@ -2,6 +2,7 @@ import React from 'react'
 import Mainscreen from '../../../components/Header/Mainscreen';
 import { Link } from 'react-router-dom';
 import { Button, Card } from 'react-bootstrap';
+import notes, {} from '../../../data/notes';
 
 const MyNotes = () => {
   return (
@@ -10,27 +11,33 @@ const MyNotes = () => {
             <Button style={{marginLeft: 10, marginBottom: 6}} size='lg'>
                 Create New Note
             </Button> 
-            <Card style={{margin: 10}}>
-                <Card.Header style={{display: "flex"}}>
-                  <span
-                    style={{
-                      color: "black",
-                      textDecoration: "none",
-                      flex: 1,
-                      cursor: "pointer",
-                      alignSelf: "center",
-                      fontSize: 18,
-                    }}
-                  >
-                    title
-                  </span>
-                  <div>
-                    <Button>Edit</Button>
-                    <Button variant='danger' className='mx-2'>Delete</Button>
-                  </div>
-                </Card.Header>
-              </Card>
+              {notes.map(note=> (
+                  <Card style={{margin: 10}}>
+                    <Card.Header style={{display: "flex"}}>
+                      <span
+                        style={{
+                          color: "black",
+                          textDecoration: "none",
+                          flex: 1,
+                          cursor: "pointer",
+                          alignSelf: "center",
+                          fontSize: 18,
+                        }}
+                      >
+                        {note.title}
+                      </span>
+                      <div>
+                        <Button>Edit</Button>
+                        <Button variant='danger' className='mx-2'>Delete</Button>
+                      </div>
+                    </Card.Header>
+                  </Card>
+    
+                ))
+              
+              }
 
+            
         </Link>
     </Mainscreen>  
 
