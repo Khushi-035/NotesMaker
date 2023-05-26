aa1dfe3584684f927c4f78b1e44d8e8ee93849dd
Khushi Sachan <sachan.khushi02@gmail.com>
2023-05-26 17:14:13 +0530

    "Cards Added"

--- frontend/src/screens/Landing Page/MyNotes/MyNotes.js
+++ frontend/src/screens/Landing Page/MyNotes/MyNotes.js
@@ -10,9 +10,30 @@ const MyNotes = () => {
             <Button style={{marginLeft: 10, marginBottom: 6}} size='lg'>
                 Create New Note
             </Button> 
+            <Card style={{margin: 10}}>
+                <Card.Header style={{display: "flex"}}>
+                  <span
+                    style={{
+                      color: "black",
+                      textDecoration: "none",
+                      flex: 1,
+                      cursor: "pointer",
+                      alignSelf: "center",
+                      fontSize: 18,
+                    }}
+                  >
+                    title
+                  </span>
+                  <div>
+                    <Button>Edit</Button>
+                    <Button variant='danger' className='mx-2'>Delete</Button>
+                  </div>
+                </Card.Header>
+              </Card>
+
         </Link>
     </Mainscreen>  
-    
+
   );
 };
 
